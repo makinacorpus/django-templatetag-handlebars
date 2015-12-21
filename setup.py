2f7cfe9cb43d9ed33a44cfc2ccab958da41ced18
Tadas Dailyda <tadas@dailyda.com>
2015-12-21 17:01:22 +0200

    py3 compat

--- setup.py
+++ setup.py
@@ -11,19 +11,20 @@ setup(
     url='https://github.com/makinacorpus/django-templatetag-handlebars',
     download_url="http://pypi.python.org/pypi/django-templatetag-handlebars/",
     description="Easily embed Handlebars.js templates in your django templates",
-    long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' + 
+    long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
                      open(os.path.join(here, 'CHANGES')).read(),
     license='LPGL, see LICENSE file.',
     install_requires=['django'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    classifiers=['Topic :: Utilities', 
+    classifiers=['Topic :: Utilities',
                  'Natural Language :: English',
                  'Operating System :: OS Independent',
                  'Intended Audience :: Developers',
                  'Environment :: Web Environment',
                  'Framework :: Django',
                  'Development Status :: 5 - Production/Stable',
-                 'Programming Language :: Python :: 2.7'],
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3'],
 )
--- templatetag_handlebars/templatetags/templatetag_handlebars.py
+++ templatetag_handlebars/templatetags/templatetag_handlebars.py
@@ -1,7 +1,6 @@
-from django.conf import settings
 from django import template
 from django.conf import settings
-
+from django.utils import six
 
 register = template.Library()
 
@@ -9,7 +8,7 @@ register = template.Library()
 
     Most of this code was written by Miguel Araujo
     https://gist.github.com/893408
-    
+
 """
 
 def verbatim_tags(parser, token, endtagname):
@@ -17,7 +16,7 @@ def verbatim_tags(parser, token, endtagname):
     Javascript templates (jquery, handlebars.js, mustache.js) use constructs like:
 
     ::
-    
+
         {{if condition}} print something{{/if}}
 
     This, of course, completely screws up Django templates,
@@ -53,7 +52,7 @@ def verbatim_tags(parser, token, endtagname):
                 parser.invalid_block_tag(token, command, None)
             try:
                 node = compile_func(parser, token)
-            except template.TemplateSyntaxError, e:
+            except template.TemplateSyntaxError as e:
                 if not parser.compile_function_error(token, e):
                     raise
             text_and_nodes.append(node)
@@ -69,21 +68,21 @@ class VerbatimNode(template.Node):
     Wrap {% verbatim %} and {% endverbatim %} around a
     block of javascript template and this will try its best
     to output the contents with no changes.
-    
+
     ::
-    
+
         {% verbatim %}
             {% trans "Your name is" %} {{first}} {{last}}
         {% endverbatim %}
     """
     def __init__(self, text_and_nodes):
         self.text_and_nodes = text_and_nodes
-    
+
     def render(self, context):
         output = ""
         # If its text we concatenate it, otherwise it's a node and we render it
         for bit in self.text_and_nodes:
-            if isinstance(bit, basestring): 
+            if isinstance(bit, six.string_types):
                 output += bit
             else:
                 output += bit.render(context)
@@ -104,20 +103,20 @@ class HandlebarsNode(VerbatimNode):
     """
     A Handlebars.js block is a *verbatim* block wrapped inside a
     named (``template_id``) <script> tag.
-    
+
     ::
-    
+
         {% tplhandlebars "tpl-popup" %}
             {{#ranges}}
                 <li>{{min}} < {{max}}</li>
             {{/ranges}}
         {% endtplhandlebars %}
-    
+
     """
     def __init__(self, template_id, text_and_nodes):
         super(HandlebarsNode, self).__init__(text_and_nodes)
         self.template_id = template_id
-    
+
     def render(self, context):
         USE_EMBER_STYLE_ATTRS = getattr(settings, 'USE_EMBER_STYLE_ATTRS', False)
         output = super(HandlebarsNode, self).render(context)
@@ -136,5 +135,6 @@ def tplhandlebars(parser, token):
     try:
         tag_name, template_id = map(stripquote , tokens[:2])
     except ValueError:
-        raise template.TemplateSyntaxError, "%s tag requires exactly one argument" % token.split_contents()[0]
+        raise template.TemplateSyntaxError(
+            "%s tag requires exactly one argument" % token.split_contents()[0])
     return HandlebarsNode(template_id, text_and_nodes)
